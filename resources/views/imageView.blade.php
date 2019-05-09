411213a2b202bb9be50f3386cc8f7ca522084a37
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-09 23:19:24 +0100

    Minor changes

--- resources/views/imageView.blade.php
+++ resources/views/imageView.blade.php
@@ -7,10 +7,14 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.2/css/fileinput.css" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/css/fileinput.css"
+              media="all" rel="stylesheet" style="text/css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+              media="all" rel="stylesheet" style="text/css">
+        <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/fontawesome.min.css"
+              media="all" rel="stylesheet" style="text/css"> -->
+
         <style>
             .main-section {
                 margin: 0 auto;
@@ -28,30 +32,30 @@
                     <h1>Multiple upload Images</h1>
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                     <div class="form-group">
-                        <input type="file" id="imageFile" name="file" multiple class="file"
+                        <input type="file" id="image-1" name="file" multiple class="file"
                             data-overwrite-initial="false" data-min-file-count="2">
                     </div>
                 </div>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"
                 type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.2/js/fileinput.js" 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/js/fileinput.js"
                 type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.0.2/themes/fa/theme.js" 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/themes/fa/theme.js" 
                 type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/popper.min.js" 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" 
                 type="text/javascript"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" 
                 type="text/javascript"></script>
 
         <script type="text/javascript">
-            $("#imageFile").fileinput({
+            $("#image-1").fileinput({
                 theme: 'fa',
                 uploadUrl: "/image-submit",
                 uploadExtraData:function(){
                     return {
-                        _token:$("input[name='_token]").val()
+                        _token:$("input[name='_token']").val()
                     };
                 },
 

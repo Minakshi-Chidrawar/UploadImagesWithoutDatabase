7acf4d806f6a8d4cf5bbce5acc295b2662dc9e29
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-12 21:33:31 +0100

    View images

--- resources/views/viewImage.blade.php
+++ resources/views/viewImage.blade.php
@@ -2,13 +2,17 @@
     
     <div class="carousel-inner" role="listbox">
 
-        @foreach($images as $image)
-            @if ($image != "")
-
-            <div class="item">
-                <img src= "/upload/12052019/{{ $image }}" width="200px" height="200px">
+        @foreach(array_chunk($images, 3) as $items)
+            <div class="row">
+                @foreach($items as $item)
+                <div class="col-sm-4 portfolio-item">
+                    <div>
+                        <img  class="img-responsive" style="border: 2px; margin: 10px;" src= "/upload/12052019/{{ $item }}" width="200px" height="200px">
+                    </div>
+                </div>
+                @endforeach
             </div>
-            @endif
+
         @endforeach
 
     </div>
